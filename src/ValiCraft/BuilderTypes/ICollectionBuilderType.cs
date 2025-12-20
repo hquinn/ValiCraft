c24eff24bfb34b65ca00b4dcca5b3d4ba11d9b08
Hayden Quinn <hayden@increnaut.io>
2025-12-20 11:37:25 +1000

    feat: Reverted type-constrained builder interfaces

--- src/ValiCraft/BuilderTypes/ICollectionBuilderType.cs
+++
@@ -1,9 +0,0 @@
-namespace ValiCraft.BuilderTypes;
-
-/// <summary>
-/// A builder type for collection properties.
-/// Collection validation rules like HasMinCount, HasItems, etc. extend this interface
-/// to ensure they only appear in IntelliSense for collection properties.
-/// </summary>
-public interface ICollectionBuilderType<TRequest, TItem> : IBuilderType<TRequest, IEnumerable<TItem>?>
-    where TRequest : class;
--- src/ValiCraft/BuilderTypes/IComparableBuilderType.cs
+++
@@ -1,9 +0,0 @@
-namespace ValiCraft.BuilderTypes;
-
-/// <summary>
-/// A builder type for properties that implement IComparable.
-/// Comparison rules like IsGreaterThan, IsBetween, etc. extend this interface.
-/// </summary>
-public interface IComparableBuilderType<TRequest, TTarget> : IBuilderType<TRequest, TTarget>
-    where TRequest : class
-    where TTarget : IComparable<TTarget>;
